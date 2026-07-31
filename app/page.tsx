ca90b8d0c4bba81549505a4baa48eddb1dc785c7
Janidu Pasan <janidupasan2@gmail.com>
2026-07-31 18:01:29 +0530

    update to the vinod

--- app/page.tsx
+++ app/page.tsx
@@ -282,7 +282,7 @@ export default function Home() {
     },
     {
       name: "Vinod Samarawickrama",
-      role: "Co-Founder & Director",
+      role: "Co-Founder",
       image: "https://i.ibb.co/n7xdCBm/8-1.jpg",
       social: {
         linkedin: "https://www.linkedin.com/in/vinod-samarawickrama-767bb273/"
